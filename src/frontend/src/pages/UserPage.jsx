7555c26de7429f5db71e9942c89a0a29df75ef37
Zeidan chabo <zeidanchabo@gmail.com>
2026-02-04 14:38:15 -0500

    add authentication features

--- src/frontend/src/pages/UserPage.jsx
+++
@@ -1,9 +0,0 @@
-import PreferencesForm from "../components/PreferencesForm";
-
-export default function UserPage() {
-    return (
-        <div className="User-Page">
-            <PreferencesForm />
-        </div>
-    );
-}
